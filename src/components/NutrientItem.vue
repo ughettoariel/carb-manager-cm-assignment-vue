7cdb5b54b7d7dda1bb533eeb07fd28bbb2b9653e
Ariel Ughetto <ariel.ughetto@array.com>
2022-06-16 19:16:21 -0300

    Refactor recipes veiw into smaller components

--- src/components/NutrientItem.vue
+++ src/components/NutrientItem.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="nutrient-item">
-    <div v-if="name">{{ name }}</div>
-    <span> {{ value }} </span>
+    <div v-if="name" class="nutrient-name">{{ name }}</div>
+    <span class="nutrient-value"> {{ value }} </span>
   </div>
 </template>
 
 <script>
 export default {
+  name: "NutrientItem",
   props: ["name", "value"],
 };
 </script>
 
-<style scoped>
-.nutrient-item {
-  text-align: right;
-  min-width: 65px;
-  font-weight: 500;
-  color: #6f7379;
+<style lang="scss" scoped>
+.nutrient {
+  &-item {
+    text-align: right;
+    min-width: 65px;
+    font-weight: 500;
+    color: #6f7379;
 
-  div {
+    &--carbs .nutrient-name {
+      color: #eb5350;
+    }
+
+    &--fat .nutrient-name {
+      color: #ec9e31;
+    }
+
+    &--protein .nutrient-name {
+      color: #3b77b1;
+    }
+
+    &--energy .nutrient-name {
+      color: #282c37;
+    }
+  }
+
+  &-value {
     font-size: 16px;
   }
 }
